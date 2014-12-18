b01411b5a2890b6737cb4b5118b212a1f6381abc
Jeremy Brown <jeremy@notmessenger.com>
2014-12-17 23:29:37 -0600

    Add dependency on Moustrap

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-konami'
+    name: 'ember-konami',
+
+    included: function( app ) {
+        this._super.included( app );
+
+        app.import({
+            development : 'bower_components/mousetrap/mousetrap.js',
+            production  : 'bower_components/mousetrap/mousetrap.min.js'
+        });
+    }
 };
