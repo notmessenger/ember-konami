9f498b541440dd878325a9dbde15251c4d28083c
Jeremy Brown <jeremy@notmessenger.com>
2015-01-11 17:08:50 -0600

    Closes notmessenger/ember-konami#8

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -2,5 +2,42 @@ import Ember from 'ember';
 import Konami from 'ember-konami/mixins/konami';
 
 export default Ember.Controller.extend( Konami, {
-    easterEgg: 'turnDownForWhat'
+
+    /**
+     * Default egg
+     *
+     * @property {easterEgg}
+     * @type {String}
+     * @default 'raptor'
+     */
+    easterEgg: 'raptor',
+
+    /**
+     * Egg possibilities
+     *
+     * @property {eggs}
+     * @type {Array}
+     * @default [ 'raptor', 'kickAss', 'fontBomb', 'katamariHack', 'cornify', 'turnDownForWhat' ]
+     */
+    eggs: [ 'raptor', 'kickAss', 'fontBomb', 'katamariHack', 'cornify', 'turnDownForWhat' ],
+
+    /**
+     * Value selected from select list
+     *
+     * @property {selectedName}
+     * @type {String}
+     * @default 'raptor'
+     */
+    selectedName: 'raptor',
+
+    /**
+     * Set easter egg to selection value
+     *
+     * @type function
+     * @observes selectedName
+     * @return {void}
+     */
+    whichEgg: function() {
+        this.set( 'easterEgg', this.get( 'selectedName' ) );
+    }.observes( 'selectedName' )
 });
